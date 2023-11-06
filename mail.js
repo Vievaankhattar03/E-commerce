9a7c55c9aa4e5108a254a85a8ebd3978b7849bcf
Vievaan Khattar <97469460+Vievaankhattar03@users.noreply.github.com>
2023-11-06 16:27:14 +0530

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7jt-NCr-c_g8UbcmK9F7rlccdjKP-2AE",
+    authDomain: "e-commerce-dd6ef.firebaseapp.com",
+    databaseURL: "https://e-commerce-dd6ef-default-rtdb.firebaseio.com",
+    projectId: "e-commerce-dd6ef",
+    storageBucket: "e-commerce-dd6ef.appspot.com",
+    messagingSenderId: "833507885247",
+    appId: "1:833507885247:web:b43159478a9bc777af060a"
+  };
